a0fa2b7f0df15e53710662d72a0aaa1a9dc7e579
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-05-31 12:51:00 +0530

    styled components demo using CSS in JS files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import { CustomButton, Heading, OutlineButton } from './styledComponents';
 
-const App = () => {
-  return <h1> App Component</h1>
-}
+const App = () => (
+  // return <h1> App Component</h1>
+    <>
+        <Heading> App Component</Heading>
+        {/* <CustomButton type='button'> Click </CustomButton> */}
+        {/* <OutlineButton type='button'> Click </OutlineButton> */}
+        {/* we can reuse existing component without creating new one again like below */}
+        {/* <CustomButton type='button' color='#ffffff' bgColor='#0070c1'> Click </CustomButton>
+        <CustomButton type='button' color='#0070c1' bgColor='#ffffff'> Click </CustomButton> */}
+
+        {/* Conditional Styling */}
+        {/* <CustomButton type='button' outline={false}> Click </CustomButton>
+        <CustomButton type='button' outline={true}> Click </CustomButton> */}
+
+        {/* make still better code like below */}
+        {/* by using boolean attributes like below */}
+        <CustomButton type='button' outline> Click </CustomButton>
+        <CustomButton type='button'> Click </CustomButton>
+    </>
+)
 
 export default App;
